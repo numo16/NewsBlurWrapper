3dc186c4f6d3cc2a2d405cb02814f7ea1c00d8df
Ales <ales.daneu@gmail.com>
2013-05-26 19:30:23 +1000

    added mark all feeds as read

---
+++ Responses/MarkAllFeedsAsReadResponse.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace Ayls.NewsBlur.Responses
+{
+    class MarkAllFeedsAsReadResponse
+    {
+        [JsonProperty("code")]
+        public int Code { get; set; }
+
+        public bool AreMarkedAsRead
+        {
+            get { return Code == 1; }
+        }
+    }
+}
---
+++ Results/MarkAllFeedsAsReadResult.cs
@@ -0,0 +1,15 @@
+﻿namespace Ayls.NewsBlur.Results
+{
+    public class MarkAllFeedsAsReadResult : ApiCallResult
+    {
+        internal MarkAllFeedsAsReadResult()
+        {
+            Status = ApiCallStatus.Ok;
+        }
+
+        internal MarkAllFeedsAsReadResult(string error, ApiCallStatus status)
+            : base(error, status)
+        {
+        }
+    }
+}
